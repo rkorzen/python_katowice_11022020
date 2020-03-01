37fbba9c48abd9a448caf83d29b0462b1f6a80cc
rkorzen <korzeniewski@gmail.com>
2020-03-01 11:37:53 +0100

    CSV

--- dzien_8/pracownicy.py
+++ dzien_8/pracownicy.py
@@ -1,5 +1,8 @@
 import json
-source_format = "json"  # "csv"
+import csv
+
+source_format = "csv"  # "csv"
+
 
 def wypisz_pracownikow(pracownicy):
     for i, pracownik in enumerate(pracownicy, start=1):
@@ -7,6 +10,7 @@ def wypisz_pracownikow(pracownicy):
             f" - [{i}] {pracownik['imie']} {pracownik['nazwisko']} - rok: "
             f"{pracownik['rok_ur']}, pensja: {pracownik['pensja']}"
         )
+
 
 def dodaj_pracownika(pracownicy):
     imie = input("Podaj imię: ")
@@ -24,21 +28,57 @@ def dodaj_pracownika(pracownicy):
     pracownicy.append(pracownik)
     return pracownicy
 
+
+def read_json():
+    try:
+        with open("pracownicy.json") as f:
+            dane = json.load(f)
+    except FileNotFoundError:
+        dane = []
+    return dane
+
+
+def read_csv():
+    dane = []
+    try:
+        with open("pracownicy.csv") as f:
+            reader = csv.DictReader(f, delimiter=";")
+            for row in reader:
+                dane.append(row)
+    except FileNotFoundError:
+        pass
+    return dane
+
+
 def odczytaj_dane(format):
     if format == "json":
-        try:
-            with open("pracownicy.json") as f:
-                dane = json.load(f)
-        except FileNotFoundError:
-            dane = []
+        dane = read_json()
+    elif format == "csv":
+        dane = read_csv()
     else:
         raise NotImplementedError("Ten format nie jest obsługiwany")
     return dane
 
-def zapisz_dane(format):
+
+def to_json(dane):
+    with open("pracownicy.json", "w") as f:
+        json.dump(dane, f)
+
+
+def to_csv(dane):
+    with open("pracownicy.csv", "w", newline="") as f:
+        fieldnames = ["imie", "nazwisko", "rok_ur", "pensja"]
+        writer = csv.DictWriter(f, delimiter=";", fieldnames=fieldnames)
+        writer.writeheader()
+        for row in dane:
+            writer.writerow(row)
+
+
+def zapisz_dane(format, dane):
     if format == "json":
-        with open("pracownicy.json", "w") as f:
-            json.dump(pracownicy, f)
+        to_json(dane)
+    elif format == "csv":
+        to_csv(dane)
     else:
         raise NotImplementedError("Nieobsługiwany format")
     print("Dane zapisano!")
@@ -49,7 +89,6 @@ polecenie = input("Co chcesz zrobić? [d-dodać, w-wypisać]")
 
 if polecenie == 'd':
     pracownicy = dodaj_pracownika(pracownicy)
-
-    zapisz_dane(pracownicy)
+    zapisz_dane(source_format, pracownicy)
 elif polecenie == "w":
     wypisz_pracownikow(pracownicy)
